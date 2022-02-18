bf75b177bcb3f9fd4094395a2a84c5faaaa77f06
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-02-18 16:09:42 -0300

    chore: test cors

--- src/pages/api/hotmart/webhook.ts
+++ src/pages/api/hotmart/webhook.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { apiBackend } from '../../../services/api';
+import Cors from 'cors';
 
 type WebhookHotmart = {
   data: {
@@ -32,23 +33,41 @@ type WebhookHotmart = {
   version: string;
 };
 
+const cors = Cors({
+  methods: ['POST'],
+});
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === 'POST') {
+    await runMiddleware(req, res, cors);
     try {
       const webhook: WebhookHotmart = req.body;
       console.log(JSON.stringify(webhook));
 
       const { data } = webhook as WebhookHotmart;
 
-      apiBackend.post('/purchase', {
-        product: data.product.name,
-        transaction: data.purchase.transaction,
-        status: data.purchase.status,
-        email: data.buyer.email,
-      });
+      apiBackend
+        .post('/purchase', {
+          product: data.product.name,
+          transaction: data.purchase.transaction,
+          status: data.purchase.status,
+          email: data.buyer.email,
+        })
+        .then(response => console.log(response));
     } catch (error) {
       console.log(error);
     }
