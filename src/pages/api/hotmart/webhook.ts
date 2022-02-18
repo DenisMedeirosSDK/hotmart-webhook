8c21260bf85eebb79188a272691b27fd28b4f153
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2022-02-18 16:41:17 -0300

    chore: add sentry and remove cors

--- src/pages/api/hotmart/webhook.ts
+++ src/pages/api/hotmart/webhook.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-// import { apiBackend } from '../../../services/api';
-// import Cors from 'cors';
+import { withSentry } from '@sentry/nextjs';
+import { apiBackend } from '../../../services/api';
 
 type WebhookHotmart = {
   data: {
@@ -33,42 +33,24 @@ type WebhookHotmart = {
   version: string;
 };
 
-// const cors = Cors({
-//   methods: ['POST'],
-// });
-
-// function runMiddleware(req, res, fn) {
-//   return new Promise((resolve, reject) => {
-//     fn(req, res, result => {
-//       if (result instanceof Error) {
-//         return reject(result);
-//       }
-
-//       return resolve(result);
-//     });
-//   });
-// }
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    // await runMiddleware(req, res, cors);
     const webhook: WebhookHotmart = req.body;
-    console.log(JSON.stringify(webhook));
 
-    // const { data } = webhook as WebhookHotmart;
+    const { data } = webhook;
 
-    // apiBackend
-    //   .post('/purchase', {
-    //     product: data.product.name,
-    //     transaction: data.purchase.transaction,
-    //     status: data.purchase.status,
-    //     email: data.buyer.email,
-    //   })
-    //   .then(response => console.log(response));
+    await apiBackend.post('/purchase', {
+      product: data.product.name,
+      transaction: data.purchase.transaction,
+      status: data.purchase.status,
+      email: data.buyer.email,
+    });
+
+    return res.send('');
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method not allowed');
   }
 }
+
+export default withSentry(handler);
